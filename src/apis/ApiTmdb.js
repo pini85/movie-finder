9d963cf048b2d0e968b88a51ecf0fa45c00d4aee
Pinchas Hodadad <pinchas.hodadad@gmail.com>
2020-03-01 15:07:19 +0200

    added tmdb api

---
+++ src/apis/ApiTmdb.js
@@ -0,0 +1,16 @@
+const ApiKey = "3e296e6f6a1b142633468c58b584ab9b";
+export const ApiTmdbQuery = async query => {
+  const response = await fetch(
+    `https://api.themoviedb.org/3/search/movie?api_key=${ApiKey}&language=en-US&query=star%20wars&page=1&include_adult=false`
+  );
+  const data = await response.json();
+  return data;
+};
+export const ApiTmdbId = async id => {
+  const response = await fetch(
+    `
+    https://api.themoviedb.org/3/movie/${id}?api_key=${ApiKey}&language=en-US`
+  );
+  const data = await response.json();
+  return data;
+};
