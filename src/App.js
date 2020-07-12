3ce72fe694ece04acd41f5d6076518725fc64b04
Pinchas Hodadad <pinchas.hodadad@gmail.com>
2020-07-12 16:09:50 +0300

    added scrollintoview component to react router

--- src/App.js
+++ src/App.js
@@ -14,28 +14,34 @@ import ActorMovies from "./components/ActorMovies/ActorMovies.component";
 import PopularActors from "./components/PopularActors/PopularActors.component";
 import Customize from "./components/Customize/Customize.component";
 import Film from "./components/spinners/Film/Film.component";
+import ScrollIntoView from "./components/ScrollIntoView.component";
 
 const App = ({ theme }) => {
   return (
     <div id="app" className={theme}>
       <Router>
-        <Navbar />
-        <Switch>
-          <Route exact path="/" component={Home} />
-          <Route path="/movie/:id" component={ShowMovie} />
-          <Route path="/movies/:category/page/:page" component={Movies} />
-          <Route path="/tv/:category/page/:page" component={TvShows} />
-          <Route path="/search/:query/page/:page" component={MovieListSearch} />
-          <Route exact path="/advanced-search" component={AdvancedSearch} />
-          <Route
-            path="/advanced-search/:query/page/:page"
-            component={MovieListSearch}
-          />
-          <Route path="/popular-actors/:query" component={PopularActors} />
-          <Route path="/actors/:query/page/:page" component={ActorMovies} />
-          <Route exact path="/customize" component={Customize} />
-          <Route exact path="/spinner/" component={Film} />
-        </Switch>
+        <ScrollIntoView>
+          <Navbar />
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route path="/movie/:id" component={ShowMovie} />
+            <Route path="/movies/:category/page/:page" component={Movies} />
+            <Route path="/tv/:category/page/:page" component={TvShows} />
+            <Route
+              path="/search/:query/page/:page"
+              component={MovieListSearch}
+            />
+            <Route exact path="/advanced-search" component={AdvancedSearch} />
+            <Route
+              path="/advanced-search/:query/page/:page"
+              component={MovieListSearch}
+            />
+            <Route path="/popular-actors/:query" component={PopularActors} />
+            <Route path="/actors/:query/page/:page" component={ActorMovies} />
+            <Route exact path="/customize" component={Customize} />
+            <Route exact path="/spinner/" component={Film} />
+          </Switch>
+        </ScrollIntoView>
       </Router>
     </div>
   );
